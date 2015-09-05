ba7e57283fd390b2c1c73b151a1e9c9f8f180eab
MR.Y <programming@yatsukouji.com>
2015-09-05 14:15:56 +0900

    newのルーティングとindexのルーティングを一部修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'items/index' => 'items#index'
   get 'items/:id' => 'items#show'
+  get 'items' => 'items#index'
+  get 'item' => 'items#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
