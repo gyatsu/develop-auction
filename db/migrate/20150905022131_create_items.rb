8053a2f4a19f23a91da4ac43a0aef9744b95d990
MR.Y <programming@yatsukouji.com>
2015-09-05 11:36:05 +0900

    コントローラ作成 rails g controller items

--- db/migrate/20150905022131_create_items.rb
+++ db/migrate/20150905022131_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
-      t.text :detail
+      t.text :details
       t.integer :price
       t.integer :seller_id
       t.string :mail
